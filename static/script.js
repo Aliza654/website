1ca29f8638fbe0b9d9601bc0502ab99619dbe08a
Aliza654 <alizaaijazd@gmail.com>
2026-05-05 17:20:58 +0500

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,11 @@
+function showMessage() {
+    alert("Welcome to Doll Paradise ??");
+}
+
+function addToCart(doll) {
+    alert(doll + " added to cart!");
+}
+
+function sendMessage() {
+    alert("Message sent successfully!");
+}
